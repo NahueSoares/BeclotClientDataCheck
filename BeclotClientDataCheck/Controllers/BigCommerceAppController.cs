5eccb8e39fc2d0d8fcb4a13643b81cabda4c9b31
NahueSoares <nahuel.soares@paqtechsa.com>
2026-04-16 13:06:42 -0300

    16_4_2026

---
+++ BeclotClientDataCheck/Controllers/BigCommerceAppController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeclotClientDataCheck.Controllers
+{
+    [ApiController]
+    [Route("api/bigcommerce")]
+    public class BigCommerceAppController : ControllerBase
+    {
+        [HttpGet("auth")]
+        public IActionResult Auth()
+        {
+            return Ok("BigCommerce auth endpoint ready");
+        }
+
+        [HttpGet("load")]
+        public IActionResult Load()
+        {
+            return Ok("BigCommerce load endpoint ready");
+        }
+
+        [HttpGet("uninstall")]
+        [HttpPost("uninstall")]
+        public IActionResult Uninstall()
+        {
+            return Ok("BigCommerce uninstall endpoint ready");
+        }
+    }
+}
